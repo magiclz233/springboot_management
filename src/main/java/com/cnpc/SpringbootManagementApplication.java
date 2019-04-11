68fbb9dead8982bc860a8273eaf7ae1784f5f7ef
magic <magiclz233@163.com>
2019-04-11 17:21:47 +0800

    基于Spring Boot的用户管理系统

---
+++ src/main/java/com/cnpc/SpringbootManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cnpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringbootManagementApplication.class, args );
+    }
+
+}
